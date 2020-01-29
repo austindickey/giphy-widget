a0e58fbe7df57f3bc5f10db85fe335fb09cff39e
austindickey <austindickey@me.com>
2020-01-28 22:27:56 -0600

    succesfully generates buttons from array and generates 10 gifs per which button was clicked along with the rating

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,7 +1,33 @@
 var topics = ["dog", "horse", "wolf", "panther", "lion", "bear", "kangaroo"]
 
-var animal = ""
-
 var apiKey = "Ixd4dkIi2CAphy1CFWUfbbGvDOZV0b0k"
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
+var animal
+
+var queryURL
+
+var newButton
+
+var newGif
+
+for (var i = 0; i < topics.length; i++) {
+    newButton = $("#animalButtons").append("<button class='buttonGroup' data-name='" + topics[i] + "'>" + topics[i] + "</button>")
+}
+
+$(".buttonGroup").on("click", function(){
+
+    $("#animalDisplay").empty()
+    animal = $(this).data("name")
+    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response.data[0]);
+        for (var z = 0; z < 10; z++) {
+            newGif = $("#animalDisplay").append("<embed src='" + response.data[z].embed_url + "' class='gifs'><span class='rating'>" + response.data[z].rating + "<span>")
+        }
+    })
+})
+
