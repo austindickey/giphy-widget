6490d5b72f5953af734bc75a0a3115707bed12c5
austindickey <austindickey@me.com>
2020-01-28 23:48:35 -0600

    js logic is completed and working

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,15 +1,7 @@
 var topics = ["dog", "horse", "wolf", "panther", "lion", "bear", "kangaroo"]
-
 var apiKey = "Ixd4dkIi2CAphy1CFWUfbbGvDOZV0b0k"
-
-var animal
-
-var queryURL
-
 var newButton
-
 var newGif
-
 var userInput = ""
 
 for (var i = 0; i < topics.length; i++) {
@@ -30,20 +22,36 @@ function createButtons() {
 }
 
 function callAPI() {
+
     $(".buttonGroup").on("click", function() {
 
         $("#animalDisplay").empty()
-        animal = $(this).data("name")
-        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
+        
+        var animal = $(this).data("name")
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
     
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response.data[0]);
+
             for (var z = 0; z < 10; z++) {
-                newGif = $("#animalDisplay").append("<embed src='" + response.data[z].embed_url + "' class='gifs'><span class='rating'>" + response.data[z].rating + "<span>")
+                newGif = $("#animalDisplay").append("<img src='" + response.data[z].images.fixed_height_still.url + "' class='gifs' data-index='" + z + "'><span class='rating'>" + response.data[z].rating + "<span>")
             }
+            
+            $(".gifs").on("click", function() {
+
+                var picURL = $(this).attr("src")
+                var gifIndex = $(this).data("index")
+          
+                if (picURL === response.data[gifIndex].images.fixed_height_still.url) {
+                    $(this).attr("src", response.data[gifIndex].images.fixed_height.url)
+                } else {
+                    $(this).attr("src", response.data[gifIndex].images.fixed_height_still.url)
+                }
+                
+            });
+
         })
     })
 }
