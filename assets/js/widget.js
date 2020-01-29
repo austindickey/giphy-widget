2b4a301197ebf26ec135de983af896f2de19135f
austindickey <austindickey@me.com>
2020-01-28 23:05:44 -0600

    you can now add a new working button

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -10,24 +10,42 @@ var newButton
 
 var newGif
 
+var userInput = ""
+
 for (var i = 0; i < topics.length; i++) {
     newButton = $("#animalButtons").append("<button class='buttonGroup' data-name='" + topics[i] + "'>" + topics[i] + "</button>")
 }
 
-$(".buttonGroup").on("click", function(){
-
-    $("#animalDisplay").empty()
-    animal = $(this).data("name")
-    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response.data[0]);
-        for (var z = 0; z < 10; z++) {
-            newGif = $("#animalDisplay").append("<embed src='" + response.data[z].embed_url + "' class='gifs'><span class='rating'>" + response.data[z].rating + "<span>")
-        }
-    })
+$("#submitButton").on("click", function() {
+    userInput = $("#userInput").val()
+    topics.push(userInput)
+    createButtons()
 })
 
+function createButtons() {
+    for (var y = 7; y < topics.length; y++) {
+        newButton = $("#animalButtons").append("<button class='buttonGroup' data-name='" + topics[y] + "'>" + topics[y] + "</button>")
+    }
+    callAPI()
+}
+
+function callAPI() {
+    $(".buttonGroup").on("click", function() {
+
+        $("#animalDisplay").empty()
+        animal = $(this).data("name")
+        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.data[0]);
+            for (var z = 0; z < 10; z++) {
+                newGif = $("#animalDisplay").append("<embed src='" + response.data[z].embed_url + "' class='gifs'><span class='rating'>" + response.data[z].rating + "<span>")
+            }
+        })
+    })
+}
+
+callAPI()
