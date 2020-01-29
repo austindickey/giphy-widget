c8f52a06b378b1093997d182d42be51a75005001
austindickey <austindickey@me.com>
2020-01-28 21:11:45 -0600

    adding placeholder tags to html and for the js file adding starter array and api link

---
+++ assets/js/widget.js
@@ -0,0 +1,7 @@
+var topics = ["dog", "horse", "wolf", "panther", "lion", "bear", "kangaroo"]
+
+var animal = ""
+
+var apiKey = "Ixd4dkIi2CAphy1CFWUfbbGvDOZV0b0k"
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
