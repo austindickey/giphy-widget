4b40dae2dfe007c98ad22923ff7896d1918a4a53
austindickey <austindickey@me.com>
2020-04-07 21:55:05 -0500

    updating front end look, and removing search by button aspect

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,73 +1,51 @@
-var topics = ["dog", "horse", "wolf", "panther", "lion", "bear", "kangaroo"]
 var apiKey = "Ixd4dkIi2CAphy1CFWUfbbGvDOZV0b0k"
-var newButton
 var newGif
-var userInput = ""
-
-for (var i = 0; i < topics.length; i++) {
-    newButton = $("#animalButtons").append("<button class='buttonGroup' data-name='" + topics[i] + "'>" + topics[i] + "</button>")
-}
-
-$("#submitButton").on("click", function() {
-    userInput = $("#userInput").val()
-    topics.push(userInput)
-    createButtons()
-    $("#userInput").val("")
-})
-
-function createButtons() {
-
-    $("#animalButtons").empty()
-
-    for (var y = 0; y < topics.length; y++) {
-        newButton = $("#animalButtons").append("<button class='buttonGroup' data-name='" + topics[y] + "'>" + topics[y] + "</button>")
-    }
-    callAPI()
-}
 
 function callAPI() {
 
-    $(".buttonGroup").on("click", function() {
+    $("#gifDisplay").empty()
 
-        $("#animalDisplay").empty()
-        
-        var animal = $(this).data("name")
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + animal + "&limit=10&offset=0&rating=PG&lang=en"
-    
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
+    var searchTerm = $("#userInput").val()
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=10&offset=0&rating=R&lang=en"
 
-            for (var z = 0; z < 10; z++) {
-                var containerDiv = $("<div>")
-                containerDiv.addClass("individualGifs")
-                var newImg = $("<img>")
-                newImg.attr("src", response.data[z].images.fixed_height_still.url)
-                newImg.addClass("gifs")
-                newImg.attr("data-index", z)
-                var rating = $("<p>")
-                rating.text("Rating: " + response.data[z].rating)
-                containerDiv.append(newImg)
-                containerDiv.append(rating)
-                $("#animalDisplay").append(containerDiv)
+    $("#userInput").val("")
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        for (var z = 0; z < 10; z++) {
+            var containerDiv = $("<div>")
+            containerDiv.addClass("individualGifs")
+            var newImg = $("<img>")
+            newImg.attr("src", response.data[z].images.fixed_height_still.url)
+            newImg.addClass("gifs")
+            newImg.attr("data-index", z)
+            var rating = $("<p>")
+            rating.addClass("rating")
+            rating.text("Rating: " + response.data[z].rating)
+            containerDiv.append(newImg)
+            containerDiv.append(rating)
+            $("#gifDisplay").append(containerDiv)
+        }
+
+        $(".gifs").on("click", function () {
+
+            var picURL = $(this).attr("src")
+            var gifIndex = $(this).data("index")
+
+            if (picURL === response.data[gifIndex].images.fixed_height_still.url) {
+                $(this).attr("src", response.data[gifIndex].images.fixed_height.url)
+            } else {
+                $(this).attr("src", response.data[gifIndex].images.fixed_height_still.url)
             }
-            
-            $(".gifs").on("click", function() {
-
-                var picURL = $(this).attr("src")
-                var gifIndex = $(this).data("index")
-          
-                if (picURL === response.data[gifIndex].images.fixed_height_still.url) {
-                    $(this).attr("src", response.data[gifIndex].images.fixed_height.url)
-                } else {
-                    $(this).attr("src", response.data[gifIndex].images.fixed_height_still.url)
-                }
-                
-            });
 
         })
+
     })
 }
 
-callAPI()
+$("#submitButton").on("click", function () {
+    callAPI()
+})
